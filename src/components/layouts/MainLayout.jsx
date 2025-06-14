538f2b10302a591b29aa466c269b18f6c520f6e4
s-alif <alif160038@gmail.com>
2025-06-14 23:49:42 +0600

    ADD - responsive css

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -1,9 +1,17 @@
-import {Outlet} from "react-router";
+import {Outlet} from "react-router"; // it is used to pass a component as children from the route itself
+import Navbar from "../Navbar.jsx";
+import Footer from "../Footer.jsx";
 
 
 const MainLayout = () => {
     return (
-        <div>{<Outlet />}</div>
+        <>
+            <Navbar />
+            <main className={"main-layout"}>
+                <Outlet />
+            </main>
+            <Footer/>
+        </>
     )
 }
 
