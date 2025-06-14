a3fc08782235498b33751b7d06ff26f3c8f35f59
s-alif <alif160038@gmail.com>
2025-06-15 01:57:24 +0600

    DONE - home page - home,about sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,33 @@
+import {Link} from "react-router";
+import logo from "./assets/images/logo.png"
+
 const App = () => {
     return (
-        <div className="">Hello world</div>
+        <section className="page-section" id={"home-page"}>
+            
+            {/*home section*/}
+            <section id="home">
+                <div className="container">
+                    <div className="home-content p-3">
+                        <h1 className="home-title text-white">Discover Delicious Recipes</h1>
+                        <p className="home-subtitle text-white">Cook tasty meals with step-by-step guides.</p>
+                        <Link to="/recipe-list/" className="btn bg-primary link home-btn text-black">Explore Now</Link>
+                    </div>
+                </div>
+            </section>
+            
+            {/*about section*/}
+            <section id="about" className="section-layout about">
+                <div className="container">
+                    <div className="content">
+                        <h2 className={"title"}>About Us</h2>
+                        <img src={logo} alt="logo" />
+                        <p>At <b>RecipeHub</b>, we're passionate about bringing people together through the joy of cooking. Whether you're a beginner or an experienced chef, our collection of easy-to-follow recipes is designed to help you create delicious meals with confidence. We believe good food should be simple, fun, and shared with loved ones</p>
+                    </div>
+                </div>
+            </section>
+    
+        </section>
     )
 }
 
