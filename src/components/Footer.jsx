2e0c0119004a5717ad21da0a13c1d01d37dfb806
s-alif <alif160038@gmail.com>
2025-06-15 19:40:19 +0600

    DONE - footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,13 @@
+import {Copyright} from "lucide-react";
+import {Link} from "react-router";
+
 export const Footer = () => {
     return (
-        <footer>Footer</footer>
+        <footer className="footer">
+            <div>
+                <Copyright /> Copyright 2025 <Link to={"/"} className={"link text-white fw-bold"}>RecipeHub</Link> | All Rights Reserved
+            </div>
+        </footer>
     )
 }
 
